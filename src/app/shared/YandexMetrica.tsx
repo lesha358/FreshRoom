b6b9f7513ab0e6debcb60ccb9c18449b23abd406
lesha358 <lesha358@example.com>
2025-09-25 21:59:37 +0300

    Fix Yandex Metrica: use external script instead of inline code to avoid CSP issues

--- src/app/shared/YandexMetrica.tsx
+++ src/app/shared/YandexMetrica.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Script from 'next/script'
 
 interface YandexMetricaProps {
@@ -6,7 +7,6 @@ interface YandexMetricaProps {
 }
 
 export default function YandexMetrica({ counterId, variant = 'full' }: YandexMetricaProps) {
-  // Не рендерим компонент если нет ID счетчика
   if (!counterId) {
     return null
   }
@@ -15,26 +15,24 @@ export default function YandexMetrica({ counterId, variant = 'full' }: YandexMet
     <>
       {variant !== 'noscriptOnly' && (
         <Script
-          id="yandex-metrica"
+          id="yandex-metrica-src"
+          src="https://mc.yandex.ru/metrika/tag.js"
           strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-              m[i].l=1*new Date();
-              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a))
-              (window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
-
-              ym(${counterId}, 'init', {
-                ssr: true,
-                webvisor: true,
-                clickmap: true,
-                ecommerce: 'dataLayer',
-                accurateTrackBounce: true,
-                trackLinks: true,
-                defer: true
-              });
-            `,
+          onLoad={() => {
+            try {
+              const w = window as unknown as { ym?: (...args: unknown[]) => void }
+              if (typeof w !== 'undefined' && typeof w.ym === 'function') {
+                w.ym(Number(counterId), 'init', {
+                  ssr: true,
+                  webvisor: true,
+                  clickmap: true,
+                  ecommerce: 'dataLayer',
+                  accurateTrackBounce: true,
+                  trackLinks: true,
+                  defer: true
+                });
+              }
+            } catch {}
           }}
         />
       )}
