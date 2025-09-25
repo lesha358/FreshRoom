4ecb0f18fa4ea27d0f9cb2403330e682866171c5
lesha358 <lesha358@example.com>
2025-09-25 22:05:10 +0300

    Simplify Yandex Metrica: use standard implementation in head/body instead of complex component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./shared/Header";
 import StructuredData from "./shared/StructuredData";
-import YandexMetrica from "./shared/YandexMetrica";
 
 const inter = Inter({ subsets: ["latin", "cyrillic"], variable: "--font-inter" });
 
@@ -115,11 +114,48 @@ export default function RootLayout({
         <link rel="preconnect" href="https://www.googletagmanager.com" crossOrigin="anonymous" />
         <link rel="dns-prefetch" href="//mc.yandex.ru" />
         <link rel="dns-prefetch" href="//www.googletagmanager.com" />
+        
+        {/* Яндекс.Метрика */}
+        {process.env.NEXT_PUBLIC_YANDEX_METRICA_ID && (
+          <>
+            <script
+              type="text/javascript"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                  (window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
+                  
+                  ym(${process.env.NEXT_PUBLIC_YANDEX_METRICA_ID}, 'init', {
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+                  });
+                `,
+              }}
+            />
+          </>
+        )}
       </head>
       <body className={`${inter.variable}`}>
+        {/* Яндекс.Метрика noscript */}
+        {process.env.NEXT_PUBLIC_YANDEX_METRICA_ID && (
+          <noscript>
+            <div>
+              <img 
+                src={`https://mc.yandex.ru/watch/${process.env.NEXT_PUBLIC_YANDEX_METRICA_ID}`} 
+                style={{position:'absolute', left:'-9999px'}} 
+                alt="" 
+              />
+            </div>
+          </noscript>
+        )}
+        
         <StructuredData type="WebSite" />
         <StructuredData type="LocalBusiness" />
-        <YandexMetrica counterId={process.env.NEXT_PUBLIC_YANDEX_METRICA_ID || ''} />
         <Header />
         {children}
       </body>
